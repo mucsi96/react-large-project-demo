d2d684461e1675c206d970c882a8a295ca8c1863
Igor Mucsicska <mucsi_96@yahoo.com>
2020-07-17 14:15:21 +0200

    Move storybook configuration to dev-tools

--- packages/dev-tools/bin/index.js
+++ packages/dev-tools/bin/index.js
@@ -2,15 +2,18 @@
 const script = process.argv[2];
 const { resolve } = require("path");
 
+process.argv = [process.argv[0], process.argv[1]];
+
 switch (script) {
   case "storybook": {
+    const customPresets = require("@storybook/core/dist/server/common/custom-presets.js");
+    customPresets.default = () => [require.resolve("../.storybook/main")];
     require("@storybook/react/bin");
     return;
   }
   case "test": {
     process.argv = [
-      process.argv[0],
-      process.argv[1],
+      ...process.argv,
       "test",
       "--config-overrides",
       require.resolve("../config-overrides"),
--- packages/ui-lib/.storybook/main.js
+++
@@ -1,1 +0,0 @@
-module.exports = require("dev-tools/storybook.config");
