6808c9b5d10d068a4d8f3778fcddc479def706b1
Igor Bari <mucsi96@gmail.com>
2021-01-20 11:51:10 +0100

    Add prepare

--- packages/dev-tools/config/rollup.config.js
+++ packages/dev-tools/config/rollup.config.js
@@ -1,48 +1,50 @@
-import commonjs from "@rollup/plugin-commonjs";
-import resolve from "@rollup/plugin-node-resolve";
-import typescript from "@rollup/plugin-typescript";
-import url from "@rollup/plugin-url";
-import svgr from "@svgr/rollup";
-import { dirname, relative } from "path";
-import external from "rollup-plugin-peer-deps-external";
-import postcss from "rollup-plugin-postcss";
-import visualizer from "rollup-plugin-visualizer";
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import url from '@rollup/plugin-url';
+import svgr from '@svgr/rollup';
+import { dirname, relative } from 'path';
+import external from 'rollup-plugin-peer-deps-external';
+import postcss from 'rollup-plugin-postcss';
+import visualizer from 'rollup-plugin-visualizer';
 
-const relativeBasePath = relative(__dirname, process.cwd()).replace(/\\/g, "/");
+const relativeBasePath = relative(__dirname, process.cwd()).replace(/\\/g, '/');
 const packageJson = require(`${relativeBasePath}/package.json`);
 
-const extensions = [".js", ".jsx", ".ts", ".tsx"];
+const extensions = ['.js', '.jsx', '.ts', '.tsx'];
 const config = {
-  input: "src",
+  input: 'src',
   output: [
     {
       file: packageJson.main,
-      format: "cjs",
+      format: 'cjs',
     },
     {
       dir: dirname(packageJson.module),
-      format: "esm",
+      format: 'esm',
       sourcemap: true,
       preserveModules: true,
-      preserveModulesRoot: "src",
+      preserveModulesRoot: 'src',
     },
   ],
   plugins: [
     typescript({
       exclude: [
-        "**/*.stories.tsx",
-        "**/*.spec.ts",
-        "**/*.spec.tsx",
-        "./test/**/*",
+        '**/*.stories.tsx',
+        '**/*.spec.ts',
+        '**/*.spec.tsx',
+        './test/**/*',
       ],
       declaration: true,
       declarationDir: dirname(packageJson.types),
-      rootDir: "src",
-      module: "ESNext",
-      target: "ES2015",
+      rootDir: 'src',
+      module: 'ESNext',
+      target: 'ES2015',
     }),
     postcss(),
-    external(),
+    external({
+      includeDependencies: true,
+    }),
     resolve({
       extensions,
     }),
