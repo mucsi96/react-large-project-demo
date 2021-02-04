1660684401743e883bb85fa549855e7dd8020ee0
Igor Bari <mucsi96@gmail.com>
2021-02-04 21:35:11 +0100

    Step 0

--- packages/friends-api/src/friends.ts
+++
@@ -1,6 +0,0 @@
-import { fetchJSON } from 'core';
-import { Friend } from './types';
-
-export function getFriends(): Promise<Friend[]> {
-  return fetchJSON('/api/friends');
-}
--- packages/friends/.eslintrc.js
+++
@@ -1,1 +0,0 @@
-module.exports = require('dev-tools/eslint').default(__dirname);
